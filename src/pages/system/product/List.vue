6529d912e14dfdad9f695f9f8ed2696f4b4745da
1002840547 <1002840547@qq.com>
2020-01-04 13:43:34 +0800

    3

--- src/pages/system/product/List.vue
+++ src/pages/system/product/List.vue
@@ -1,11 +1,147 @@
 <template>
     <div>
-        产品管理
+        <!-- 2 -->
+        <!-- 按钮 -->
+        <el-button type = "success" size = "small" @click="toAddHandler">添加</el-button>
+        <el-button type = "danger" size = "small">删除</el-button>
+        <!-- /按钮 -->
+        <!-- 表格 -->
+        <el-table :data="customers">
+            <el-table-column prop = "id" label = "编号"></el-table-column>
+            <el-table-column prop = "name" label = "产品名称"></el-table-column>
+            <el-table-column prop = "price" label = "价格"></el-table-column>
+            <el-table-column prop = "status" label = "状态"></el-table-column>
+            <el-table-column prop = "photo" label = "图片"></el-table-column>
+            <el-table-column prop = "categoryId" label = "种类编号"></el-table-column>
+            <el-table-column label = "操作">
+                <template v-slot="slot">
+                    <a href="" @click.prevent="toDeleteHandler(slot.row.id)">删除</a>
+                    <a href="" @click.prevent="toUpdateHandler(slot.row)">修改</a>
+                </template>
+            </el-table-column>
+        </el-table>
+        <!-- /表格 -->
+        <!-- 分页 -->
+        <!-- <el-pagination layout="prev, pager, next" :total="50"></el-pagination> -->
+        <!-- /分页 -->
+        <!-- 模态框 -->
+        <el-dialog 
+            title="录入产品信息" 
+            :visible.sync="visible" 
+            width="60%" >
+            <el-form :model="form" label-width="80px">
+                <el-form-item label="产品名称">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="价格">
+                    <el-input v-model="form.price"></el-input>
+                </el-form-item>
+                <el-form-item label="状态">
+                    <el-input v-model="form.status"></el-input>
+                </el-form-item>
+                <el-form-item label="种类编号">
+                    <el-input v-model="form.categoryId"></el-input>
+                </el-form-item>
+            </el-form>
+         <span></span>
+         <span slot="footer" class="dialog-footer">
+            <el-button @click="closeModalHandler" size = "small">取 消</el-button>
+            <el-button type="primary" @click="submitHandler" size = "small">确 定</el-button>
+        </span>
+        </el-dialog>
+        <!-- /模态框 -->
     </div>
 </template>
 
 <script>
+import request from '@/utils/request'
+import querystring from 'querystring'
+export default {
+    //用于存放网页中需要调用的方法
+    methods:{
+        loadData(){
+            let url = "http://localhost:6677/customer/findAll"
+        request.get(url).then((response)=>{
+            //将查询结果设置到customers中，this指向外部函数的this
+            this.customers = response.data;
+        })
+        },
+        submitHandler(){
+            //调用request与后台进行交互，并且要携带参数
+            let url = "http://localhost:6677/customer/saveOrUpdate"
+            request({
+                url,
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/x-www-form-urlencoded"
+                },
+                data:querystring.stringify(this.form)
+            }).then((response)=>{
+                //模态框关闭
+                this.closeModalHandler();
+                //刷新
+                this.loadData();
+                //提示信息
+                this.$message({
+                    type:"success",
+                    message:response.message    
+                })
+            })
+        },
+        toDeleteHandler(id){
+            //确认
+            this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+            //调用后台接口，完成删除操作
+            let url = "http://localhost:6677/customer/deleteById?id="+id;
+            request.get(url).then((response)=>{
+                //刷新数据
+                this.loadData();
+                //提示结果
+                this.$message({
+                    type: 'success',
+                    message: response.message
+                });
+            })
 
+          
+        })
+        },
+        toUpdateHandler(row){
+            //模态框的表单中显示出当前行的信息
+            this.form = row;
+            this.visible = true;
+        },
+        closeModalHandler(){
+            this.visible = false;
+        },
+        toAddHandler(){
+            //将form变为初始值
+            this.form = {
+                type:"customer"
+            }
+            this.visible = true;
+        }
+    },
+    //用于存放要向网页中显示的数据
+    data(){
+        return{
+            visible:false,
+            customers:[],
+            form:{
+                type:"customer"
+            }
+        }
+    },
+    created(){
+        //this为当前vue实例对象
+        //vue实例创建完毕
+        this.loadData();
+    }
+}
 </script>
 
 <style scoped>
